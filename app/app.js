e097118c93cbcfc8f4b035cb2b9b8511da79f153
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-04-28 23:53:45 -0400

    first commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+const app = express()
+
+//Servir arquivos estáticos No express. 
+app.use(express.static('public'))
+// configuração do template engine a ser utilizado na aplicação
+app.set('view engine','ejs')
+// configuração do diretório da pasta views
+app.set('views','./app/views')
+
+// Rota para iniciar a aplicação
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+export default app
---
+++ server.js
@@ -0,0 +1,6 @@
+import app from './app/app.js'
+const PORT = 3000
+
+app.listen(PORT,()=>{
+console.log(`Server is running on http://localhost://${PORT}`)
+})
