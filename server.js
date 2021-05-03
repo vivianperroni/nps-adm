6993b0f31e524eff0b8ae2f73c59c277ed8dd496
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-05-03 18:49:07 -0400

    🩹 :fix correção da URL do servidor

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import app from './app/app.js'
 const PORT = 3000
 
 app.listen(PORT,()=>{
-console.log(`Server is running on http://localhost://${PORT}`)
+console.log(`Server is running on http://localhost:${PORT}`)
 })
