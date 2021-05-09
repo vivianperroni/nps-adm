5d4afa7a4c6a3eff47990591617cbee118b14580
vivianperroni <vivian.perroni@hotmail.com>
2021-05-09 02:23:06 -0300

    Adicionar: Conexão com banco de dados #3

--- app/database/connection.js
+++ app/database/connection.js
@@ -1,1 +1,21 @@
-// code here a connection database
+const Sequelize = require('sequelize')
+const databaseConfig = require('../config/database') /* importação do arquivo da pasta 
+config que possui as configuraçõe de acesso
+*/
+
+const connection = new Sequelize(databaseConfig)
+
+module.exports = connection
+
+// testing the connection 
+testConnection = async function(){
+    try {
+        await connection.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error); 
+    }
+}
+testConnection() /* utilize apenas para testar, quando for fazer o commit no 
+repositório, remova a chamada da função
+ */
