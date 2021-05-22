095a983af7cf1be28babf08c3d6d0bf1a18641db
vivianperroni <vivian.perroni@hotmail.com>
2021-05-22 20:39:05 -0300

    🔨 Consertando erros

--- app/models/Questao.js
+++ app/models/Questao.js
@@ -5,7 +5,7 @@ class Questao extends Model{
     static init(connection){
         super.init({
             descricao:DataTypes.STRING,
-            ativo:DataTypes.TINYINT,
+            ativo:DataTypes.INTEGER,
             created_at:DataTypes.DATE,
             updated_at:DataTypes.DATE
         },
