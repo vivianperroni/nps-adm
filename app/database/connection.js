ab42bdb36a568ecee5c9573af4e0300121ea7394
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-05-24 15:04:06 -0400

    add: importação e inicialização das classes de modelo.

--- app/database/connection.js
+++ app/database/connection.js
@@ -2,7 +2,16 @@ const Sequelize = require('sequelize')
 const databaseConfig = require('../config/database') /* importação do arquivo da pasta 
 config que possui as configuraçõe de acesso
 */
+const Classificacao = require('../models/Classificacao')
+const Nps = require('../models/Nps')
+const Questao = require('../models/Questao')
+const Sugestoes = require('../models/Sugestoes')
+const Usuario = require('../models/Usuario')
 
 const connection = new Sequelize(databaseConfig)
-
+Nps.init(connection)
+Classificacao.init(connection)
+Questao.init(connection)
+Sugestoes.init(connection)
+Usuario.init(connection)
 module.exports = connection
