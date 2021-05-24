8cfc964dcfc30997351b6d5c0babfc38034b33f9
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-05-24 14:59:50 -0400

    🔥 remove code: testConnection

--- app/database/connection.js
+++ app/database/connection.js
@@ -6,16 +6,3 @@ config que possui as configuraçõe de acesso
 const connection = new Sequelize(databaseConfig)
 
 module.exports = connection
-
-// testing the connection 
-testConnection = async function(){
-    try {
-        await connection.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error); 
-    }
-}
-testConnection() /* utilize apenas para testar, quando for fazer o commit no 
-repositório, remova a chamada da função
- */
