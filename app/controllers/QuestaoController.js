079ce4fca561fe5def5554a39931dec1fc204a34
vivianperroni <vivian.perroni@hotmail.com>
2021-05-24 23:29:42 -0300

    🚧✨feat: QuestaoController - index,create,store #8

---
+++ app/controllers/QuestaoController.js
@@ -0,0 +1,24 @@
+const Questao = require('../models/Questao')
+class QuestaoController {
+    static index(req,res){
+    }
+    static create(req,res){
+        res.render('questao/create')
+    }
+    static async store(req,res){
+        let descricao = req.body.descricao
+        let ativo = true
+
+        let questao = new Questao()
+        questao.descricao = descricao
+        questao.ativo = ativo
+        await questao.save()
+        res.redirect('/questao')
+    }
+    static edit(req,res){
+    }
+    static update(req,res){
+    }
+    static destroy(req,res){
+    }        
+}
