d114e44a7fb861b7d88889d5c4d7a60764dbc57a
vivianperroni <vivian.perroni@hotmail.com>
2021-06-05 02:12:46 -0300

    🚧✨feat: QuestaoC - index,edit,update e destroy #8

--- app/controllers/QuestaoController.js
+++ app/controllers/QuestaoController.js
@@ -1,6 +1,7 @@
 const Questao = require('../models/Questao')
 class QuestaoController {
     static index(req,res){
+        Questao.findAll()
     }
     static create(req,res){
         res.render('questao/create')
@@ -16,10 +17,23 @@ class QuestaoController {
         res.redirect('/questao')
     }
     static edit(req,res){
+        Questao.findByPK(id)
     }
     static update(req,res){
+        let descricao = req.body.descricao
+        let ativo = true
+
+        Questao.findByPK(id)
+
+        questao.descricao = descricao
+		questao.ativo = ativo
+		await questao.save()
     }
     static destroy(req,res){
+        let id = req.params.id
+        let questao = await Questao.findByPK(id)
+        await questao.destroy()
+        res.redirect('/questao/')
     }        
 }
 
